7cae55c67d192c027920a6b6908319b03bd83d0b
Ioanis Palaskas <ioanis94@gmx.net>
2024-05-29 22:02:56 +0200

    Fixed: Weekdays are now in German

--- hs-app/src/app/clock/clock.component.ts
+++ hs-app/src/app/clock/clock.component.ts
@@ -1,6 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+enum Wochentage{
+  Montag = 1,
+  Dienstag,
+  Mittwoch,
+  Donnerstag,
+  Freitag,
+  Samstag,
+  Sonntag
+}
+
 @Component({
   selector: 'app-clock',
   standalone: true,
@@ -8,16 +18,21 @@ import { CommonModule } from '@angular/common';
   template: `
   <div class="time-date-box">
     <div class="time">{{time | date:'HH:mm:ss'}}</div>
-    <div class="date">{{time | date:'EEEE dd.MM.YYYY' }}</div>
+    <div class="date"> {{currentDay}} {{ time | date:'dd.MM.YYYY' }}</div>
   </div>`,
   styleUrl: './clock.component.css'
 })
+
 export class MyClock implements OnInit {
-    time: any = Date.now();
-    
+  time: any = Date.now();
+  currentDay = Wochentage[new Date().getDay()]
+
     ngOnInit(): void {
         setInterval(() => {
             this.time = Date.now();
         }, 1000);
     }
+
 }
+
+
